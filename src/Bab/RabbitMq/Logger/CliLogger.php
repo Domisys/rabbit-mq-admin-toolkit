19f72a8b7bafcc45ca8e66b33f5d38cc9d413cfe
Aymeric PLANCHE <planche@domisys.com>
2015-01-15 15:05:57 +0100

    feat: use verbosity for log level message

--- src/Bab/RabbitMq/Logger/CliLogger.php
+++ src/Bab/RabbitMq/Logger/CliLogger.php
@@ -4,18 +4,26 @@ namespace Bab\RabbitMq\Logger;
 
 use Symfony\Component\Console\Output\OutputInterface;
 use Psr\Log\AbstractLogger;
+use Psr\Log\LogLevel;
 
 class CliLogger extends AbstractLogger
 {
     private $output;
-    
+
     public function __construct(OutputInterface $output)
     {
         $this->output = $output;
     }
-    
+
     public function log($level, $message, array $context = array())
     {
-        $this->output->writeln($message);
+        $verbosity = $this->output->getVerbosity();
+
+        if($verbosity >= OutputInterface::VERBOSITY_NORMAL && $level === LogLevel::ERROR
+        || $verbosity >= OutputInterface::VERBOSITY_VERBOSE && $level === LogLevel::INFO
+        || $verbosity >= OutputInterface::VERBOSITY_VERY_VERBOSE)
+        {
+            $this->output->writeln($message);
+        }
     }
 }
